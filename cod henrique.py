e6b02c1a327c35957ad9004e1dd9a07dbc6204f1
Noth430 <henrique2.095@outlook.com>
2024-10-19 20:13:14 +0000

    salvar

--- cod henrique.py
+++ cod henrique.py
@@ -1,17 +1,24 @@
-import os 
-from dataclasses import dataclass
+def calcular_inss(salario_base):
+    if salario_base <= 1100:
+        return salario_base * 0.075
+    elif salario_base <= 2203.48:
+        return salario_base * 0.09
+    elif salario_base <= 3305.22:
+        return salario_base * 0.12
+    elif salario_base <= 6433.57:
+        return salario_base * 0.14
+    else:
+        return 854.36
 
-@dataclass
-class Funcionario:
-    maticula: str
-    senha: int
-    salario_b: float
-
-def desconto_inss(resposta):
-    lista_salario = []
-
-    if resposta <= 1.100:
+def calacular_irrf(salario_base):
+    if salario_base <= 2259.20:
+        return salario_base + print("isento: ")
+    elif salario_base >= 2259.21 or salario_base <= 2826.65:
+        return salario_base * 0.075
+    elif salario_base >= 2826.65 or salario_base <= 3751.06:
+        return salario_base * 0.15
+    elif salario_base >= 3751.06 or salario_base <= 4664.68:
+        return salario_base *0.22
+    elif salario_base >= 4664.68:
+        return salario_base * 0.00275
         
-        lista_salario.append(resposta)
-        
-
